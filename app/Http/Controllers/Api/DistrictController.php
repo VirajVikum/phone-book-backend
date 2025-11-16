b39e8a05fd4fb8a69de61cb83c507d1c6c0a7ed9
Viraj Vikum <virajempire2001@gmail.com>
2025-11-16 14:17:18 +0530

    developed user register functions

--- app/Http/Controllers/Api/DistrictController.php
+++ app/Http/Controllers/Api/DistrictController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\District;
 use Illuminate\Http\Request;
 
 class DistrictController extends Controller
 {
-    //
+    public function index() {
+        return response()->json(District::all());
+    }
+
+    public function towns($id) {
+        return response()->json(District::findOrFail($id)->towns);
+    }
 }
--- app/Http/Controllers/Api/ProviderController.php
+++ app/Http/Controllers/Api/ProviderController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProviderTown;
+use App\Models\ServiceProvider;
 use Illuminate\Http\Request;
 
 class ProviderController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $provider = ServiceProvider::create([
+            'name' => $request->name,
+            'gender' => $request->gender,
+            'mobile_no' => $request->mobile_no,
+            'whatsapp_no' => $request->whatsapp_no,
+            'address' => $request->address,
+            'district_id' => $request->district_id,
+            'industries' => $request->industries,
+            'photo_url' => $request->photo_url ?? ''
+        ]);
+
+        foreach($request->towns as $townId){
+            ProviderTown::create(['provider_id'=>$provider->id,'town_id'=>$townId]);
+        }
+
+        return response()->json(['success' => true, 'provider_id' => $provider->id]);
+    }
+
+    public function search(Request $request)
+    {
+        $query = ServiceProvider::where('district_id', $request->district_id)
+            ->whereJsonContains('industries', $request->requirement)
+            ->whereHas('towns', fn($q) => $q->where('town_id', $request->town_id))
+            ->get();
+
+        return response()->json($query);
+    }
 }
